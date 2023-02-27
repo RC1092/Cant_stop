5ef709c73e05489a0c299a03dd87441bfe2cc5b8
emoly <emdormody@mun.ca>
2023-02-26 22:56:32 -0330

    panels finished no functionality

--- Drafts/Code/ChoosePieces.java
+++ Drafts/Code/ChoosePieces.java
@@ -2,8 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ChoosePieces extends JFrame {
+    private int numPlayers;
+    private JPanel selection;
+    private String[] shapes, colors;
     public ChoosePieces(int numPlayers){
+        this.numPlayers = numPlayers;
         this.setLayout(new BorderLayout());
+        this.shapes = new String[]{" ","Circle","Square","Triangle","Star"};
+        this.colors = new String[]{" ", "Blue","Red","Yellow","Green","Orange","Purple"};
         JPanel header = new JPanel(new FlowLayout());
         header.setBackground(Color.red);
         JButton startGame = new JButton("Start Game");
@@ -13,7 +19,7 @@ public class ChoosePieces extends JFrame {
         startGame.setForeground(Color.red);
         header.add(startGame);
 
-        JPanel selection = new JPanel();
+        selection = new JPanel();
         if (numPlayers == 2) {
             this.setSize(new Dimension(800, 500));
             selection.setLayout(new GridLayout(1,2));}
@@ -24,46 +30,79 @@ public class ChoosePieces extends JFrame {
             this.setSize(800,900);
             selection.setLayout(new GridLayout(2,2));}
 
-        for (int i=0; i<numPlayers; i++) {
-            JPanel background = new JPanel(new BorderLayout());
-            JPanel choices = new JPanel(new GridLayout(3, 2));
-            JPanel player = new JPanel(new FlowLayout());
-            JLabel playerNumber = new JLabel("Player ".concat(Integer.toString(i+1)));
+        buildChoicePanel();
 
-            playerNumber.setFont(new Font("Calibre",Font.BOLD,25));
-            playerNumber.setOpaque(true);
-            playerNumber.setBackground(Color.white);
-            playerNumber.setForeground(Color.red);
-            playerNumber.setBorder(BorderFactory.createLineBorder(Color.white));
-            player.add(playerNumber);
-
-            choices.setBorder(BorderFactory.createLineBorder(Color.white));
-            JLabel color = new JLabel("Color: ");
-            JLabel shape = new JLabel("Shape: ");
-            JLabel name = new JLabel("Name: ");
-            formatLabel(color);
-            choices.add(color);
-            formatLabel(shape);
-            choices.add(shape);
-            formatLabel(name);
-            choices.add(name);
-            background.add(choices, BorderLayout.CENTER);
-            background.add(player, BorderLayout.NORTH);
-            selection.add(background);}
         getContentPane().add(header, BorderLayout.NORTH);
         getContentPane().add(selection, BorderLayout.CENTER);
 
-
         setResizable(false);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         }
-        private void formatLabel(JLabel label){
+
+    private void formatLabel(JLabel label){
             label.setFont(new Font("Calibre",Font.BOLD,25));
             label.setOpaque(true);
             label.setBackground(Color.red);
             label.setForeground(Color.white);
-            label.setBorder(BorderFactory.createLineBorder(Color.white));
+            //label.setBorder(BorderFactory.createLineBorder(Color.white));
+        }
+        private void buildChoicePanel(){
+            for (int i=0; i<numPlayers; i++) {
+                JPanel background = new JPanel(new BorderLayout());
+                JPanel choices = new JPanel(new GridLayout(3, 1));
+                JPanel player = new JPanel(new FlowLayout());
+                JLabel playerNumber = new JLabel("Player ".concat(Integer.toString(i+1)));
+
+                playerNumber.setFont(new Font("Calibre",Font.BOLD,25));
+                playerNumber.setOpaque(true);
+                playerNumber.setBackground(Color.white);
+                playerNumber.setForeground(Color.red);
+                playerNumber.setBorder(BorderFactory.createLineBorder(Color.white));
+                player.add(playerNumber);
+                player.setBackground(Color.white);
+
+                choices.setBorder(BorderFactory.createLineBorder(Color.white));
+                JLabel color = new JLabel("Color: ");
+                JLabel shape = new JLabel("Shape: ");
+                JLabel name = new JLabel("Name: ");
+                formatLabel(color);
+                formatLabel(shape);
+                formatLabel(name);
+
+                background.add(choices, BorderLayout.CENTER);
+                background.add(player, BorderLayout.NORTH);
+                selection.add(background);
+
+                JComboBox<String> chooseShape = new JComboBox<>(shapes);
+                chooseShape.setFont(new Font("Calibre",Font.BOLD,20));
+                chooseShape.setBackground(Color.white);
+                JComboBox<String> chooseColor = new JComboBox<>(colors);
+                chooseColor.setFont(new Font("Calibre",Font.BOLD,20));
+                chooseColor.setBackground(Color.white);
+                JTextField chooseName = new JTextField("Type your name here");
+                chooseName.setFont(new Font("Calibre",Font.PLAIN,20));
+                chooseName.setBackground(Color.white);
+
+                JPanel panel = new JPanel(new FlowLayout());
+                panel.setBackground(Color.red);
+                choices.add(panel);
+                panel.add(shape);
+                panel.add(chooseShape);
+
+                JPanel panel2 = new JPanel(new FlowLayout());
+                panel2.setBackground(Color.red);
+                choices.add(panel2);
+                panel2.add(color);
+                panel2.add(chooseColor);
+
+                JPanel panel3 = new JPanel(new FlowLayout());
+                panel3.setBackground(Color.red);
+                choices.add(panel3);
+                panel3.add(name);
+                panel3.add(chooseName);
+            }
         }
     }
 

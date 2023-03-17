367749b79d5b6d7b5c8d8278ea7de022a53d8f16
NathanQuintonMun <113479441+NathanQuintonMun@users.noreply.github.com>
2023-03-17 15:55:22 -0230

    Some getters and setters

--- Drafts/Code/Game.java
+++ Drafts/Code/Game.java
@@ -9,20 +9,12 @@ public class Game {
 
     //Stores the turn order as a key value pair, keys are 1-numberofplayers (e.g 1,2,3,4) indicating the order they move in. Values are the Player objects. 
     private HashMap<Integer,Player> turnOrder;
-    private ArrayList<pieces> movementPieces = new ArrayList<pieces>();
 
     public Game(ArrayList<Player> players){
         this.players = players;
         dice = new Dice();
-        turn = new Turn(players, dice);
         board = new Board(this,players);
-        while (movementPieces.size() < 3){
-            movementPieces.add(new pieces("Arrow", "White"));
-        }
-    }
-
-    public void addPlayer(Player player){
-        players.add(player);
+        turn = new Turn(players, dice, movementPieces);
     }
 
     public Turn getTurn(){
